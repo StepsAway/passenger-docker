21801ae21cf93e09cc035b2aaca87369ca96daad
JD Trout <jd.trout@stepsaway.com>
2017-04-04 09:26:58 -0700

    [INFRA-227] Bump baseimage to 2.0.1

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 NAME = stepsaway/passenger
-VERSION = 2.0.0
+VERSION = 2.0.1
 
 .PHONY: build_all clean clean_images \
 	build_ruby230 build_ruby231 build_ruby240 build_jruby1726 \
--- image/enable_repos.sh
+++ image/enable_repos.sh
@@ -5,9 +5,9 @@ set -x
 
 ## Phusion Passenger
 if [[ "$PASSENGER_ENTERPRISE" ]]; then
-	echo deb https://download:$PASSENGER_ENTERPRISE_DOWNLOAD_TOKEN@www.phusionpassenger.com/enterprise_apt trusty main > /etc/apt/sources.list.d/passenger.list
+	echo deb https://download:$PASSENGER_ENTERPRISE_DOWNLOAD_TOKEN@www.phusionpassenger.com/enterprise_apt xenial main > /etc/apt/sources.list.d/passenger.list
 else
-	echo deb https://oss-binaries.phusionpassenger.com/apt/passenger trusty main > /etc/apt/sources.list.d/passenger.list
+	echo deb https://oss-binaries.phusionpassenger.com/apt/passenger xenial main > /etc/apt/sources.list.d/passenger.list
 fi
 
 # updated the added repos
